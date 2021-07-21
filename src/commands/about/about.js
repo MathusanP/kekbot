5d64dadbba25626ce820095e6ce1b504fa1da911
agnag <mathuyt141@gmail.com>
2021-07-21 21:31:08 +0100

    Fixed that incredibly misleading thing

--- src/commands/about/about.js
+++ src/commands/about/about.js
@@ -9,7 +9,7 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`Hello, I'm kekbot!`)
-            .setDescription(`I was made in the 18th of july by <@732667572448657539>\nTo get started, please use kekhelp`)
+            .setDescription(`I was made in the 18th of july by Bagel#1475 To get started, please use kekhelp`)
             .setColor(`RANDOM`)
             .setThumbnail(`https://cdn.discordapp.com/avatars/734007071686787123/c6864719c028d71e47d3f860f39f0ce1.webp`);
 
