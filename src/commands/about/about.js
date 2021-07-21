db437e471a113aa7a3dd4ee68e11e0dfd5a3de0d
agnag <mathuyt141@gmail.com>
2021-07-21 20:15:14 +0100

    Fixed about.js

--- src/commands/about/about.js
+++ src/commands/about/about.js
@@ -1,19 +1,19 @@
+/* eslint-disable indent */
 const Discord = require('discord.js');
 
 module.exports = {
-	name: `about`,
-	description: 'About the bot',
-	arguments: 0,
-	execute(message) {
+    name: `about`,
+    description: 'About the bot',
+    arguments: 0,
+    execute(message) {
 
-		const embed = new Discord.MessageEmbed()
-			.setTitle(`Hello, I'm kekbot!`)
-			.addField(`I was made in the 18th of july by <@732667572448657539> `)
-			.addField('To get started, please use kekhelp')
-			.setColor(`RANDOM`)
-			.setThumbnail(`https://cdn.discordapp.com/avatars/734007071686787123/c6864719c028d71e47d3f860f39f0ce1.webp`);
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`Hello, I'm kekbot!`)
+            .setDescription(`I was made in the 18th of july by <@732667572448657539>\nTo get started, please use kekhelp`)
+            .setColor(`RANDOM`)
+            .setThumbnail(`https://cdn.discordapp.com/avatars/734007071686787123/c6864719c028d71e47d3f860f39f0ce1.webp`);
 
-		message.channel.send(embed);
+        message.channel.send(embed);
 
-	}
+    }
 };
