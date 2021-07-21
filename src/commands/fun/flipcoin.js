664ed25c8828fa5d19d13e31386b1dd97665c763
agnag <mathuyt141@gmail.com>
2021-07-21 22:38:00 +0100

    Improved coinflip command

--- src/commands/fun/flipcoin.js
+++ src/commands/fun/flipcoin.js
@@ -1,11 +1,26 @@
 const coinflip = ["Heads", "Tails"];
-
+const Discord = require('discord.js');
 module.exports = {
 	name: 'flipcoin',
 	description: '',
 	arguments: 0,
 	async execute(message) {
 
-		message.channel.send(`${coinflip[Math.floor(Math.random() * [coinflip.length])]}`);
+		let result = (`${coinflip[Math.floor(Math.random() * [coinflip.length])]}`);
+		let member = message.author;
+
+		const flipcoinEmbed = new Discord.MessageEmbed()
+			.setTitle(`**${result}**`)
+			.setColor("RANDOM")
+			.setFooter(`${member.username}`)
+			.setTimestamp();
+		if (result === "Heads") {
+			flipcoinEmbed.setThumbnail("https://www.royalmint.com/globalassets/the-royal-mint/images/pages/new-pound-coin/large_new_pound.png");
+		}
+		else if (result === "Tails") {
+			flipcoinEmbed.setThumbnail("https://media.wired.co.uk/photos/606da41a5113453af57347d2/master/w_1600%2Cc_limit/pound-coin.png");
+		}
+
+
 	}
 };
