929c3e22a3d423ea0aeb0cf240d9bb3d9e2422b9
agnag <mathuyt141@gmail.com>
2021-07-21 19:58:59 +0100

    Fixed angrygifs.js and added about.js

---
+++ src/commands/angrygif.js
@@ -0,0 +1,16 @@
+const { Tenorapikey } = require("../../botconfig.json");
+const fetch = require('node-fetch');
+module.exports = {
+	name: 'angrygif',
+	description: '',
+	arguments: 0,
+	async execute(message) {
+
+		let url = `https://g.tenor.com/v1/search?q=angry&key=${Tenorapikey}&limit=8`;
+		let response = await fetch(url);
+		let json = await response.json();
+		const index = Math.floor(Math.random() * json.results.length);
+		message.channel.send(json.results[index].url);
+
+	}
+};
