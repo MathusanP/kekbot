15339de3d340c71a5ecf61a503327ba6e3d88d7b
Mathusan.P <78277278+bagelwastaken@users.noreply.github.com>
2021-11-28 21:13:37 +0000

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -3,7 +3,7 @@ const execute = (guild, client) => {
 
 	client.channels.cache.get('813143654998868009').send({ content: `New server joined: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!` });
 
-	guild.systemChannel.send('Hello. I am kekbot! Thanks for inviting me! To get started please use kekhelp, otherwise please join the official server: https://discord.gg/ZjyuywNN7H');
+	// guild.systemChannel.send('Hello. I am kekbot! Thanks for inviting me! To get started please use kekhelp, otherwise please join the official server: https://discord.gg/ZjyuywNN7H');
 };
 
 export { name, execute };
