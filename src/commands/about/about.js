10a81c6bfe06d2bdb0d81be1c31f9dce787f8e19
Liam Skinner <liamdavidskinner@gmail.com>
2021-12-16 23:32:18 +0000

    Added event and command handler

---
+++ src/commands/about/about.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+	name: 'about',
+	description: 'shows lots of cool information about the bot.',
+	usage: '',
+
+	permissions: [],
+	ownerOnly: false,
+	guildOnly: true,
+
+	error: false,
+	execute: ({ interaction, client }) => {
+
+		const servers = client.guilds.cache.size;
+		const users = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+
+		const uptime = `${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}h ${Math.floor(client.uptime / 60000) % 60}m ${Math.floor(client.uptime / 1000) % 60}s`;
+
+		const embed = new MessageEmbed()
+			.setTitle('My Information')
+			.setColor('RANDOM')
+			.setDescription(`Hey, I'm **${client.user.tag}**! My prefix is: \`/\`.`)
+			.addFields(
+
+				{ name: '**Total Servers:**', value: `${servers}`, inline: true },
+				{ name: '**Total Users:**', value: `${users}`, inline: true },
+				{ name: '**Total Commands:**', value: '54', inline: true },
+		
+				{ name: '**Version:**', value: '4.0.0', inline: true },
+				{ name: '**Uptime:**', value: `${uptime}`, inline: true },
+				{ name: '**Birthday:**', value: '18/06/2020', inline: true },
+
+				{ name: '**Developers:**', value: '[Bagel#1475](https://github.com/bagelwastaken)\n**[ThatsLiamS#6950](https://github.com/ThatsLiamS)**', inline: true },
+				
+			)
+			.setFooter(`Do '/help' to get started`);
+
+		interaction.reply({ embeds: [embed] });
+
+	}
+};
---
+++ src/events/interactionCreate.js
@@ -0,0 +1,50 @@
+module.exports = {
+	name: 'interactionCreate',
+	once: false,
+
+	execute: async (interaction, client) => {
+
+		/* Is interaction a command? */
+		if (interaction.isCommand()) {
+			await interaction.referReply();
+
+			const cmd = client.commands.get(interaction.commandName);
+			if (!cmd) return;
+
+			/* Is the command working? */
+			if (cmd['error'] == true) {
+				interaction.followUp({ content: 'Sorry, this command is currently unavailable. Please try again later.', ephemeral: true });
+				return;
+			}
+
+			if (cmd['permissions'] != []) {
+				for (const permission of cmd['permissions']) {
+					/* Loops through and check permissions agasint the user */
+					if (!interaction.member.permissions.has(permission.replce(' ', '_').toUpperCase())) {
+						interaction.followUp({ content: 'Sorry, you do not have permission to run this command.', ephemeral: true });
+						return;
+					}
+				}
+			}
+			/* Is the command limited to servers only */
+			if (cmd['guildOnly'] == true) {
+				if (!interaction.member.id == interaction.guild.ownerId) {
+					interaction.followUp({ content: 'Sorry, this command can only be used within a server.', ephemeral: true });
+					return;
+				}
+			}
+			/* Is the command limited to the owner */
+			if (cmd['ownerOnly'] == true) {
+				if (!interaction.member.id == interaction.guild.ownerId) {
+					interaction.followUp({ content: 'Sorry, only the server owner can run this command.', ephemeral: true });
+					return;
+				}
+			}
+
+			/* Execute the command file */
+			await cmd.execute({ interaction, client });
+
+		}
+
+	},
+};
