3e3cadf1fd9c2e930aec38aeabfa9797374037c2
agnag <mathuyt141@gmail.com>
2022-06-24 18:16:26 +0100

    fix(guildevents): Changed channel number to send event trigger to
    Chagned it to #dev-logs

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -3,7 +3,7 @@ module.exports = {
 	once: false,
 	execute: async (guild, client) => {
 
-		client.channels.cache.get('813143654998868009').send({ content: `New server joined: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!` });
+		client.channels.cache.get('935977503363858442').send({ content: `New server joined: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!` });
 
 		// guild.systemChannel.send('Hello. I am kekbot! Thanks for inviting me! To get started please use kekhelp, otherwise please join the official server: https://discord.gg/ZjyuywNN7H');
 	},
