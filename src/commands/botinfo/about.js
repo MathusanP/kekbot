059802c85c0059bcb01ed00e09113430e815ffa1
agnag <mathuyt141@gmail.com>
2022-12-28 22:12:18 +0000

    fix(rps.js): fixed rps command
    Added the computer variable within the execute function

--- src/commands/botinfo/about.js
+++ src/commands/botinfo/about.js
@@ -32,7 +32,7 @@ module.exports = {
 				{ name: '**Total Users:**', value: `${users}`, inline: true },
 				{ name: '**Total Commands:**', value: '23', inline: true },
 
-				{ name: '**Version:**', value: '4.4.3', inline: true },
+				{ name: '**Version:**', value: '4.5.0', inline: true },
 				{ name: '**Uptime:**', value: `${uptime}`, inline: true },
 				{ name: '**Birthday:**', value: '18/07/2020', inline: true },
 
--- src/commands/games/rps.js
+++ src/commands/games/rps.js
@@ -1,8 +1,6 @@
-// This command is currently undergoing iterative testing, it will be tested as we add on to it.
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const outcomes = ['Rock', 'Paper', 'Scissors'];
-const computer = outcomes[Math.floor(Math.random() * [outcomes.length])];
+
 
 module.exports = {
 	name: 'rps',
@@ -29,8 +27,41 @@ module.exports = {
 
 	error: false,
 	execute: ({ interaction }) => {
+		const outcomes = ['Rock', 'Paper', 'Scissors'];
+		const computer = outcomes[Math.floor(Math.random() * [outcomes.length])];
 		const selection = interaction.options.getString('choice');
+		let result = '';
+
+		if (selection == 'rock' && computer == 'rock') {
+			result = 'Draw!';
+		}
+		else if (selection == 'rock' && computer == 'Paper') {
+			result = 'You lost!';
+		}
+		else if (selection == 'rock' && computer == 'Scissors') {
+			result = 'You win!';
+		}
+		else if (selection == 'scissors' && computer == 'Scissors') {
+			result = 'Draw!';
+		}
+		else if (selection == 'scissors' && computer == 'Paper') {
+			result = 'You win!';
+		}
+		else if (selection == 'scissors' && computer == 'Rock') {
+			result = 'You lost!';
+		}
+		else if (selection == 'paper' && computer == 'Paper') {
+			result = 'Draw!';
+		}
+		else if (selection == 'paper' && computer == 'Rock') {
+			result = 'You win!';
+		}
+		else if (selection == 'paper' && computer == 'Scissors') {
+			result = 'You lose!';
+		}
+
 		const rpsEmbed = new MessageEmbed()
+			.setTitle(result)
 			.setColor('#fafbfc')
 			.setDescription(`Computer chose ${computer}. You chose ${selection}.`);
 		interaction.followUp({ embeds: [rpsEmbed] });
